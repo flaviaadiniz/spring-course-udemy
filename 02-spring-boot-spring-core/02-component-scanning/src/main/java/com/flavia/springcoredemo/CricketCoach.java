ca2bc8ac7b414e9deaa496127982f3a76cd10ca8
flaviaadiniz <flaviaadiniz@gmail.com>
2023-09-18 14:43:52 -0300

    Add 02-component-scanning folder

---
+++ 02-spring-boot-spring-core/02-component-scanning/src/main/java/com/flavia/springcoredemo/CricketCoach.java
@@ -0,0 +1,13 @@
+package com.flavia.springcoredemo;
+
+import org.springframework.stereotype.Component;
+
+@Component // marks the class as a Spring Bean
+public class CricketCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 15 minutes.";
+    }
+
+}
