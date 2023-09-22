d706c92dc30adc14b40bcaab27524187353a33c9
flaviaadiniz <flaviaadiniz@gmail.com>
2023-09-22 00:00:25 -0300

    Add 4 new classes that implement Coach and use @Qualifier annotation

---
+++ 02-spring-boot-spring-core/04-qualifiers/src/main/java/com/flavia/springcoredemo/common/TennisCoach.java
@@ -0,0 +1,13 @@
+package com.flavia.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice your backhand volley";
+    }
+
+}
