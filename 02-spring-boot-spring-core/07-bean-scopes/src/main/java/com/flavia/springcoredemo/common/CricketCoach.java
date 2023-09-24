979c2885cdcc0e7a1084d4f996859d237ece0b6e
flaviaadiniz <flaviaadiniz@gmail.com>
2023-09-24 14:08:28 -0300

    Add @Scope annotation to CricketCoach class with SCOPE_PROTOTYPE

--- 02-spring-boot-spring-core/07-bean-scopes/src/main/java/com/flavia/springcoredemo/common/CricketCoach.java
+++ 02-spring-boot-spring-core/07-bean-scopes/src/main/java/com/flavia/springcoredemo/common/CricketCoach.java
@@ -1,8 +1,11 @@
 package com.flavia.springcoredemo.common;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component // marks the class as a Spring Bean
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE) // creates a new object instance for each injection
 public class CricketCoach implements Coach {
 
     public CricketCoach() {
--- 02-spring-boot-spring-core/07-bean-scopes/src/main/java/com/flavia/springcoredemo/common/TrackCoach.java
+++ 02-spring-boot-spring-core/07-bean-scopes/src/main/java/com/flavia/springcoredemo/common/TrackCoach.java
@@ -1,10 +1,9 @@
 package com.flavia.springcoredemo.common;
 
-import org.springframework.context.annotation.Lazy;
+
 import org.springframework.stereotype.Component;
 
 @Component
-@Lazy
 public class TrackCoach implements Coach {
 
     public TrackCoach() {
