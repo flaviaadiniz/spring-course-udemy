d3feef23190c2daaa527ac9aa33b2c9e7b31b779
flaviaadiniz <flaviaadiniz@gmail.com>
2023-09-25 17:59:08 -0300

    Add 08-bean-lifecycle-methods folder

---
+++ 02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/flavia/springcoredemo/common/CricketCoach.java
@@ -0,0 +1,20 @@
+package com.flavia.springcoredemo.common;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component // marks the class as a Spring Bean
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE) // creates a new object instance for each injection
+public class CricketCoach implements Coach {
+
+    public CricketCoach() {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 15 minutes.";
+    }
+
+}
