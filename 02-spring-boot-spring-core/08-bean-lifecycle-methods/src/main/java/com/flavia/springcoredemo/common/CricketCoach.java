f7d8dd524f3fbbdfcd265c7f9fed6963cc071529
flaviaadiniz <flaviaadiniz@gmail.com>
2023-09-25 19:26:27 -0300

    Add @PostConstruct and @PreDestroy annotations to CricketCoach

--- 02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/flavia/springcoredemo/common/CricketCoach.java
+++ 02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/flavia/springcoredemo/common/CricketCoach.java
@@ -1,5 +1,7 @@
 package com.flavia.springcoredemo.common;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -9,6 +11,18 @@ public class CricketCoach implements Coach {
 
     public CricketCoach() {
         System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
+    // define our init method
+    @PostConstruct
+    public void doMyStartUpStuff() {
+        System.out.println("In doMyStartUpStuff(): " + getClass().getSimpleName());
+    }
+
+    // define our destroy method
+    @PreDestroy
+    public void doMyCleanUpStuff() {
+        System.out.println("In doMyCleanUpStuff(): " + getClass().getSimpleName());
     }
 
     @Override
