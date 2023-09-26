383352677a50f56b416f73eb3e1688ba73999482
flaviaadiniz <flaviaadiniz@gmail.com>
2023-09-26 08:01:15 -0300

    Add 09-java-config-bean

---
+++ 02-spring-boot-spring-core/09-java-config-bean/src/main/java/com/flavia/springcoredemo/common/CricketCoach.java
@@ -0,0 +1,18 @@
+package com.flavia.springcoredemo.common;
+
+
+import org.springframework.stereotype.Component;
+
+@Component // marks the class as a Spring Bean
+public class CricketCoach implements Coach {
+
+    public CricketCoach() {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 15 minutes.";
+    }
+
+}
