7281d7a529b02bc701527fd544bfb31edeab0f8e
flaviaadiniz <flaviaadiniz@gmail.com>
2023-10-02 22:24:26 -0300

    Add Command Line Runner and createStudent method

--- 03-spring-boot-hibernate-jpa-crud/01-crud-demo-student/src/main/java/com/flavia/cruddemostudent/CrudDemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-crud-demo-student/src/main/java/com/flavia/cruddemostudent/CrudDemoApplication.java
@@ -1,13 +1,41 @@
 package com.flavia.cruddemostudent;
 
+import com.flavia.cruddemostudent.dao.StudentDAO;
+import com.flavia.cruddemostudent.entity.Student;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CrudDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudDemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
+
+		return runner -> {
+			createStudent(studentDAO);
+		};
+
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+
+		// create the student object
+		System.out.println("Creating new Student object...");
+		Student tempStudent = new Student("Paul", "Doe", "paul@luv2code.com");
+
+		// save the student object
+		System.out.println("Saving the student...");
+		studentDAO.save(tempStudent);
+
+		// display id of the saved student
+		System.out.println("Saved student! Generated id: " + tempStudent.getId());
+
 	}
 
 }
