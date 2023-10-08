d08f9ba5471242b250f0642b2788f995170eaa39
flaviaadiniz <flaviaadiniz@gmail.com>
2023-10-07 21:50:41 -0300

    Add method deleteAllStudents

--- 03-spring-boot-hibernate-jpa-crud/01-crud-demo-student/src/main/java/com/flavia/cruddemostudent/CrudDemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-crud-demo-student/src/main/java/com/flavia/cruddemostudent/CrudDemoApplication.java
@@ -26,9 +26,16 @@ public class CrudDemoApplication {
 			// queryForStudents(studentDAO);
 			// queryForStudentsByLasyName(studentDAO);
 			// updateStudent(studentDAO);
-			deleteStudent(studentDAO);
+			//deleteStudent(studentDAO);
+			deleteAllStudents(studentDAO);
 		};
 
+	}
+
+	private void deleteAllStudents(StudentDAO studentDAO) {
+		System.out.println("Deleting all students");
+		int numRowsDeleted = studentDAO.deleteAll();
+		System.out.println("Deleted row count: " + numRowsDeleted);
 	}
 
 	private void deleteStudent(StudentDAO studentDAO) {
