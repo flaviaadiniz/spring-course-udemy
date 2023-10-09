14a974286f1914f17e4ef453bfef54ec8f794978
flaviaadiniz <flaviaadiniz@gmail.com>
2023-10-09 16:20:22 -0300

    Add @PostConstruct and refactor getStudents method

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/flavia/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/flavia/demo/rest/StudentRestController.java
@@ -2,7 +2,9 @@ package com.flavia.demo.rest;
 
 
 import com.flavia.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,21 +15,24 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    // define endpoint for "/students" - return a list of students
+    private List<Student> theStudents;
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
 
-        List<Student> theStudents = new ArrayList<>();
+    // define @PostConstruct to load the student data ... only once!
+    @PostConstruct
+    public void loadData() {
+        theStudents = new ArrayList<>();
         theStudents.add(new Student("Colleen", "Hoover"));
         theStudents.add(new Student("Matt", "Haig"));
         theStudents.add(new Student("Agatha", "Christie"));
-
-        return theStudents;
-
     }
 
 
+    // define endpoint for "/students" - return a list of students
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        return theStudents;
+    }
 
 
 }
