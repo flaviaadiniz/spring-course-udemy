266a44ced33c6a264d5293bae9f327e1f10213f6
flaviaadiniz <flaviaadiniz@gmail.com>
2023-10-18 22:44:49 -0300

    Add method findById to RestController class

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/flavia/cruddemo/controller/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/flavia/cruddemo/controller/EmployeeRestController.java
@@ -3,6 +3,7 @@ package com.flavia.cruddemo.controller;
 import com.flavia.cruddemo.entity.Employee;
 import com.flavia.cruddemo.service.EmployeeService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,20 @@ public class EmployeeRestController {
     public List<Employee> findAll() {
         return employeeService.findAll();
     }
+
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee findById(@PathVariable int employeeId) {
+        Employee theEmployee = employeeService.findById(employeeId);
+
+        if (theEmployee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        return theEmployee;
+    }
+
+
+
 
 }
