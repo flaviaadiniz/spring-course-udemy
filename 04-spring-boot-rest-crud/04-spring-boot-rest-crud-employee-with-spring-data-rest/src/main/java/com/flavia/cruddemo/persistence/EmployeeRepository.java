46ba4c64176727876f304906576e1d2d6d88d8b1
flaviaadiniz <flaviaadiniz@gmail.com>
2023-10-20 12:10:25 -0300

    Add annotation on EmployeeRepository to expose a different endpoint

--- 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/flavia/cruddemo/persistence/EmployeeRepository.java
+++ 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/flavia/cruddemo/persistence/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.flavia.cruddemo.persistence;
 
 import com.flavia.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="members") //exposes /members as the endpoint for the repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 
