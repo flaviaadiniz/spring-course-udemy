947ba0f8d30f84f1a76ef1cda87cf5c9ff2e2f90
flaviaadiniz <flaviaadiniz@gmail.com>
2023-12-25 18:29:20 -0300

    Add security configuration to restrict URLs based on roles

--- 00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ 00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -3,7 +3,9 @@ package com.luv2code.springboot.cruddemo.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -41,12 +43,20 @@ public class DemoSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(configurer ->
                 configurer
-                    .requestMatchers(HttpMethod.GET, "api/employees").hasRole("EMPLOYEE"));
+                    .requestMatchers(HttpMethod.GET, "api/employees").hasRole("EMPLOYEE")
+                    .requestMatchers(HttpMethod.GET, "api/employees/**").hasRole("EMPLOYEE") //** works as a "wild card" for the employee id
+                    .requestMatchers(HttpMethod.POST, "api/employees").hasRole("MANAGER")
+                    .requestMatchers(HttpMethod.PUT, "api/employees").hasRole("MANAGER")
+                    .requestMatchers(HttpMethod.DELETE, "api/employees/**").hasRole("ADMIN")
+        );
 
-        http.build();
-        //finish code
+        // use HTTP basic authentication
+        http.httpBasic(Customizer.withDefaults());
 
-        return null;
+        //disable Cross Site Request Forgery (CSRF) - in general not required for stateless REST APIs that use POST, PUT, DELETE and/or PATCH
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
     }
 
 
