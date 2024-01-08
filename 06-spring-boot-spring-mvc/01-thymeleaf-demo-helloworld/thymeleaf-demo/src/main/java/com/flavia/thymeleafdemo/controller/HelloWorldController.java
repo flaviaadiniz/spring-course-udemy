bd4ead0de050a36b01824bf1f30b9d9f2e454a2f
flaviaadiniz <flaviaadiniz@gmail.com>
2024-01-08 19:35:59 -0300

    Add data to Spring MVC Model

--- 06-spring-boot-spring-mvc/01-thymeleaf-demo-helloworld/thymeleaf-demo/src/main/java/com/flavia/thymeleafdemo/controller/HelloWorldController.java
+++ 06-spring-boot-spring-mvc/01-thymeleaf-demo-helloworld/thymeleaf-demo/src/main/java/com/flavia/thymeleafdemo/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.flavia.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,9 +14,30 @@ public class HelloWorldController {
         return "helloworld-form";
     }
 
+
     // create a controller method to process the HTML form
     @RequestMapping("/processForm")
     public String processForm() {
+        return "helloworld";
+    }
+
+
+    // create a controller method to read form data and add data to the model
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest request, Model model) {
+
+        // read the request parameter from the HTML form
+        String theName = request.getParameter("studentName");
+
+        // convert the data to all caps
+        theName = theName.toUpperCase();
+
+        // create the message
+        String result = "Yo! " + theName;
+
+        // add message to the model
+        model.addAttribute("message", result);
+
         return "helloworld";
     }
 
