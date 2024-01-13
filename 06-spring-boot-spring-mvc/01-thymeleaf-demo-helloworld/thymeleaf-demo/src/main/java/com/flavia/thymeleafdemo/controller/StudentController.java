9bd20334e8f808ac2761f389d26b24b5e7bb5320
flaviaadiniz <flaviaadiniz@gmail.com>
2024-01-13 11:47:05 -0300

    Add PostMapping to process the student form and its corresponding html form

--- 06-spring-boot-spring-mvc/01-thymeleaf-demo-helloworld/thymeleaf-demo/src/main/java/com/flavia/thymeleafdemo/controller/StudentController.java
+++ 06-spring-boot-spring-mvc/01-thymeleaf-demo-helloworld/thymeleaf-demo/src/main/java/com/flavia/thymeleafdemo/controller/StudentController.java
@@ -4,6 +4,8 @@ import com.flavia.thymeleafdemo.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StudentController {
@@ -20,5 +22,15 @@ public class StudentController {
 
         return "student-form";
     }
+
+    @PostMapping("/processStudentForm")
+    public String processForm(@ModelAttribute("student") Student theStudent) {
+
+        //log the input data
+        System.out.println("The student: " + theStudent.getFirstName() + " " + theStudent.getLastName());
+
+        return "student-confirmation";
+    }
+
 
 }
