53bcdbf4af33298482d33a4c6f78a868dd7f2812
flaviaadiniz <flaviaadiniz@gmail.com>
2024-01-23 19:54:03 -0300

    Add validation for number range

--- 06-spring-boot-spring-mvc/validation-demo/validation-demo/src/main/java/com/flavia/springdemo/mvc/CustomerController.java
+++ 06-spring-boot-spring-mvc/validation-demo/validation-demo/src/main/java/com/flavia/springdemo/mvc/CustomerController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.DataBinder;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
