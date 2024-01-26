008b5a45db76a27ff8317d242f236030d0a9f9a3
flaviaadiniz <flaviaadiniz@gmail.com>
2024-01-25 23:17:09 -0300

    Add CourseCodeConstraintValidator class

---
+++ 06-spring-boot-spring-mvc/validation-demo/validation-demo/src/main/java/com/flavia/springdemo/mvc/validation/CourseCodeConstraintValidator.java
@@ -0,0 +1,19 @@
+package com.flavia.springdemo.mvc.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class CourseCodeConstraintValidator implements ConstraintValidator<CourseCode, String> {
+
+    private String coursePrefix;
+
+    @Override
+    public void initialize(CourseCode courseCode) {
+        coursePrefix = courseCode.value();
+    }
+
+    @Override
+    public boolean isValid(String code, ConstraintValidatorContext context) {
+        return code.startsWith(coursePrefix);;
+    }
+}
