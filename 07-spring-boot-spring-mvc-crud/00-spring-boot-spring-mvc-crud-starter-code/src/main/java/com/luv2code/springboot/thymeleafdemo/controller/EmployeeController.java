5347317e01cf6fc948f2aafaed3f5aae95be8237
flaviaadiniz <flaviaadiniz@gmail.com>
2024-01-29 22:14:46 -0300

    Add code to show form to save new employee and save it to database

--- 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -5,8 +5,11 @@ import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.io.StringBufferInputStream;
 import java.util.List;
 
 @Controller
@@ -30,6 +33,29 @@ public class EmployeeController {
         model.addAttribute("employees", employees);
 
         return "employees/list-employees";
+
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model model) {
+
+        // create model attribute to bind form data
+        Employee employee = new Employee();
+
+        model.addAttribute("employee", employee);
+
+        return "employees/employee-form";
+
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+
+        //save the employee
+        employeeService.save(employee);
+
+        // use a redirect to prevent duplicate submissions
+        return "redirect:/employees/list";
 
     }
 
