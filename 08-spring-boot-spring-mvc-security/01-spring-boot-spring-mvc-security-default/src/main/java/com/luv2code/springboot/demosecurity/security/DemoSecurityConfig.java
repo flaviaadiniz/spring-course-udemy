eb3f70520a629f69e24291356aa16f0f87be1cf2
flaviaadiniz <flaviaadiniz@gmail.com>
2024-02-09 19:45:53 -0300

    Fix filterChain method to allow authorized users to access information according to their role

--- 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/luv2code/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/luv2code/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -39,11 +39,16 @@ public class DemoSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.authorizeHttpRequests(configurer ->
-                        configurer.anyRequest().authenticated()).formLogin( form -> form
-                                .loginPage("/showMyLoginPage")
-                                .loginProcessingUrl("/authenticateTheUser")
-                                .permitAll()
+        http.authorizeHttpRequests
+                        (configurer -> configurer
+                                .requestMatchers("/").hasRole("EMPLOYEE")
+                                .requestMatchers("/leaders/**").hasRole("MANAGER")
+                                .requestMatchers("/systems/**").hasRole("ADMIN")
+                                .anyRequest().authenticated()).formLogin
+                                    (form -> form
+                                        .loginPage("/showMyLoginPage")
+                                        .loginProcessingUrl("/authenticateTheUser")
+                                        .permitAll()
         ).logout(LogoutConfigurer::permitAll);
 
         return http.build();
