7f3b8862af2b931783c77351fd56bceab668d254
flaviaadiniz <flaviaadiniz@gmail.com>
2024-02-08 22:47:07 -0300

    Add supporting controller code and view pages for restricting to restrict access based on roles

--- 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/luv2code/springboot/demosecurity/controller/DemoController.java
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/luv2code/springboot/demosecurity/controller/DemoController.java
@@ -11,4 +11,10 @@ public class DemoController {
         return "home";
     }
 
+    // add a request mapping for /leaders
+    @GetMapping("/leaders")
+    public String showLeaders() {
+        return "leaders";
+    }
+
 }
