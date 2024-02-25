8fb173d5645db6b154f2c95f81f8dae6857dc38c
flaviaadiniz <flaviaadiniz@gmail.com>
2024-02-25 20:15:17 -0300

    Add join fetch to retrieve instructor details along with instructor

--- 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/luv2code/cruddemo/dao/AppDAOImpl.java
+++ 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/luv2code/cruddemo/dao/AppDAOImpl.java
@@ -87,7 +87,10 @@ public class AppDAOImpl implements AppDAO {
 
         // create query
         TypedQuery<Instructor> query = entityManager.createQuery(
-                "SELECT i FROM Instructor i JOIN FETCH i.courses WHERE i.id = :data", Instructor.class);
+                "SELECT i FROM Instructor i " +
+                        "JOIN FETCH i.courses " +
+                        "JOIN FETCH i.instructorDetail " +
+                        "WHERE i.id = :data", Instructor.class);
 
         query.setParameter("data", id);
 
