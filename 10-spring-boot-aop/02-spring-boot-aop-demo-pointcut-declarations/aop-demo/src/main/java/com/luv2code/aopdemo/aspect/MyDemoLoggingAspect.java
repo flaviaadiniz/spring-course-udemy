4f2dc61581ea04a18919e5f7bc08516ac1a99eaf
flaviaadiniz <flaviaadiniz@gmail.com>
2024-03-18 18:09:08 -0300

    Add new pointcut declarations for getter and setter methods and also to combine 2 pointcuts (include package and exclude getters/setters)

--- 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/aop-demo/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/aop-demo/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -14,13 +14,30 @@ public class MyDemoLoggingAspect {
     private void forDaoPackage() {
     }
 
+    // create a pointcut for getter methods
+    @Pointcut("execution(* com.luv2code.aopdemo..*.get*(..))")
+    private void getter() {
 
-    @Before("forDaoPackage()")
+    }
+
+    // create a pointcut for setter methods
+    @Pointcut("execution(* com.luv2code.aopdemo..*.set*(..))")
+    private void setter() {
+
+    }
+
+    // create pointcut: include package ... exclude getter/setter methods
+    @Pointcut("forDaoPackage() && !(getter() || setter())")
+    private void forDaoPackageNoGettersSetters() {
+
+    }
+
+    @Before("forDaoPackageNoGettersSetters()")
     public void beforeAddAccountAdvice() {
         System.out.println("====> Executing @Before advice on method");
     }
 
-    @Before("forDaoPackage()")
+    @Before("forDaoPackageNoGettersSetters()")
     public void performApiAnalytics() {
         System.out.println("====> Performing API analytics");
     }
