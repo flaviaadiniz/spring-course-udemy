f1c88c5db64c08d36d6554d1065f72a42894a19c
flaviaadiniz <flaviaadiniz@gmail.com>
2024-03-26 19:02:24 -0300

    Add code to use @After advice

--- 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/aop-demo/src/main/java/com/luv2code/aopdemo/AopDemoApplication.java
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/aop-demo/src/main/java/com/luv2code/aopdemo/AopDemoApplication.java
@@ -19,9 +19,31 @@ public class AopDemoApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(AccountDAO accountDAO, MembershipDAO membershipDAO) {
 
-		return runner -> demoTheAfterThrowingAdcie(accountDAO);
+		return runner -> demoTheAfterAdvice(accountDAO);
 		//demoTheBeforeAdvice(accountDAO, membershipDAO);
 		//demoTheAfterReturningAdvice(accountDAO);
+		//
+	}
+
+	private void demoTheAfterAdvice(AccountDAO accountDAO) {
+
+		// call method to find the accounts
+		List<Account> accounts = null;
+
+		try {
+			// add a boolean flag to simulate exceptions
+			boolean tripWire = false;
+			accounts = accountDAO.findAccounts(tripWire);
+		} catch (Exception exception) {
+			System.out.println("\nMain Program: ... caught exception " + exception);
+		}
+
+		// display the accounts
+		System.out.println("\nMain Program: demoTheAfterThrowingAdvice");
+		System.out.println("----");
+		System.out.println(accounts);
+
+		System.out.println("\n");
 
 	}
 
@@ -31,7 +53,7 @@ public class AopDemoApplication {
 		List<Account> accounts = null;
 
 		try {
-			// add a boolean flag to simulate excepetions
+			// add a boolean flag to simulate exceptions
 			boolean tripWire = true;
 			accounts = accountDAO.findAccounts(tripWire);
 		} catch (Exception exception) {
--- 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/aop-demo/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/aop-demo/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,10 +2,7 @@ package com.luv2code.aopdemo.aspect;
 
 import com.luv2code.aopdemo.Account;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -50,10 +47,10 @@ public class MyDemoLoggingAspect {
 
         // print out which method we are advising on
         String method = joinPoint.getSignature().toShortString();
-        System.out.println("====> Executing @AfterReturning on method: " + method);
+        System.out.println("\n====> Executing @AfterReturning on method: " + method);
 
         // print out the results of the method call
-        System.out.println("====> result is: " + result);
+        System.out.println("\n====> result is: " + result);
 
         // let's post-process the data ... convert the account names to uppercase
         convertAccountNamesToUpperCase(result);
@@ -68,10 +65,19 @@ public class MyDemoLoggingAspect {
 
         // print out which method we are advising on
         String method = joinPoint.getSignature().toShortString();
-        System.out.println("====> Executing @AfterThrowing on method: " + method);
+        System.out.println("\n====> Executing @AfterThrowing on method: " + method);
 
         // log the exception
-        System.out.println("====> The exception is: " + theException);
+        System.out.println("\n====> The exception is: " + theException);
+
+    }
+
+    @After("execution (* com.luv2code.aopdemo.dao.AccountDAO.findAccounts(..))")
+    public void afterFinallyFindAccountAdvice(JoinPoint joinPoint) {
+
+        // print out which method we are advising on
+        String method = joinPoint.getSignature().toShortString();
+        System.out.println("\n====> Executing @After (finally) on method: " + method);
 
     }
 
