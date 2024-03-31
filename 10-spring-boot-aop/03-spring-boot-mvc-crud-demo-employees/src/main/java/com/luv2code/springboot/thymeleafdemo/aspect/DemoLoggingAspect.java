fb025b0bd23f14d75cbfc5598b9818fd7382824a
flaviaadiniz <flaviaadiniz@gmail.com>
2024-03-30 22:00:05 -0300

    Add @Before advice to DemoLoggingAspect

--- 10-spring-boot-aop/03-spring-boot-mvc-crud-demo-employees/src/main/java/com/luv2code/springboot/thymeleafdemo/aspect/DemoLoggingAspect.java
+++ 10-spring-boot-aop/03-spring-boot-mvc-crud-demo-employees/src/main/java/com/luv2code/springboot/thymeleafdemo/aspect/DemoLoggingAspect.java
@@ -1,6 +1,8 @@
 package com.luv2code.springboot.thymeleafdemo.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
@@ -27,10 +29,29 @@ public class DemoLoggingAspect {
     private void forDaoPackage() {
     }
 
-
     @Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
     private void forAppFlow() {
     }
+
+
+    // add @Before advice
+    @Before("forAppFlow()")
+    public void before(JoinPoint joinPoint) {
+
+        // display method we are calling
+        String theMethod = joinPoint.getSignature().toShortString();
+        logger.info("===> in @Before: calling method: " + theMethod);
+
+        // get the arguments
+        Object[] args = joinPoint.getArgs();
+
+        // loop through and display the arguments
+        for (Object tempArg : args) {
+            logger.info("===> argument: " + tempArg);
+        }
+
+    }
+
 
 
 }
